c9b3ba5b072902fc1a03d309049146b04f089a02
2021331078-Nuzhat <2021331078@student.sust.edu>
2025-11-27 12:07:55 +0600

    Add files via upload

--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
   constructor() { }
 
@@ -76,4 +76,30 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue}, totalQuantity: ${totalQuantityValue}`);
     console.log(`-----`);
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+  
+  remove(theCartItem: CartItem) {
+    
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( temCartItem => temCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
+
 }
