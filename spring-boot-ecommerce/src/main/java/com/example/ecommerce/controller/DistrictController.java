4a6b36ceac5510345bed975468ac39a6c50774da
Tayaba Kabir Richee <128034034+2021331074@users.noreply.github.com>
2025-11-30 03:02:15 +0600

    Add files via upload

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/DistrictController.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/DistrictController.java
@@ -2,14 +2,12 @@ package com.example.ecommerce.controller;
 
 import com.example.ecommerce.dao.DistrictRepository;
 import com.example.ecommerce.entity.District;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class DistrictController {
